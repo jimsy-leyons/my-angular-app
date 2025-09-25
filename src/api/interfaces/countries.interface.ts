238a6afffe4d7ca4bdf17377ee45600965652800
jimsy-leyons <jimsy.leyons@mysteryinfosolutions.com>
2025-09-25 17:31:51 +0530

    Add countries interface, model, and service; integrate ng-select in contact details

---
+++ src/api/interfaces/countries.interface.ts
@@ -0,0 +1,11 @@
+import { GENERAL_STATUS } from '../enums';
+
+export interface ICountries {
+  _id?: number;
+  iso_code?: any;
+  name_capitalized?: string;
+  name?: string;
+  iso3_code?: any;
+  iso_num_code?: number;
+  phone_code?: number;
+}
--- src/app/contact/contact-details/contact-details.component.ts
+++ src/app/contact/contact-details/contact-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { IContacts } from '@api/interfaces/contacts.interface';
 import { Contacts } from '@api/models/contacts.model';
@@ -7,13 +7,20 @@ import { ContactsService } from '@api/services/contacts.service';
 import { IMultiresult, IResponse } from '@mis/interfaces/responseobjects.interface';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule, formatDate } from '@angular/common';
+import { CountriesService } from '@api/services/countries.service';
+import { CountriesFilter } from '@api/models/countries.model';
+import { ADMIN_STATUS, GENERAL_STATUS } from '@api/enums';
+import { SortItem } from '@api/models/common/filter.model';
+import { ICountries } from '@api/interfaces/countries.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 //import { AutotoastrService } from '@mis/services/autotoastr.service';
 
 @Component({
   selector: 'app-contact-details',
   standalone: true,
   // imports: [RouterOutlet],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,  NgSelectModule ],
   templateUrl: './contact-details.component.html',
   styleUrls: ['./contact-details.component.scss'],
 })
@@ -21,7 +28,18 @@ export class ContactdetailsComponent {
   contact: any;
   dataSource: IContacts[] = [];
 
-  constructor(private router: Router, private contactService: ContactsService) {
+   //dataForm: FormGroup;
+  isLoadingCountry = false;
+   // isLoadingContacts = signal(false);
+
+  country: ICountries[] = [];
+
+  constructor(
+    private router: Router,
+    private contactService: ContactsService,
+    private countryService: CountriesService,
+        private formBuilder: FormBuilder,
+  ) {
     const navigation = this.router.getCurrentNavigation();
     this.contact = navigation?.extras?.state?.['contact']; // retrieve data
     console.log('Received contact:', this.contact);
@@ -46,6 +64,85 @@ export class ContactdetailsComponent {
   //       }
   //     });
   // }
+
+  ngOnInit() {
+    this.getCountries();
+  }
+
+  async searchCountry(event: any) {
+    if (event?.term && event.term.length > 0) {
+      this.getCountries(event?.term);
+    }
+  }
+
+  async getCountries(search = '') {
+    this.isLoadingCountry = true;
+  // this.isLoadingCountry.set(true);
+    let filter: CountriesFilter = {
+      search: search,
+      searchColumns: '_id,name',
+      sort: [new SortItem('name', 'ASC')],
+      pageLength: 0,
+    };
+    this.countryService
+      .getAll(filter)
+      .subscribe((dataResponse: IResponse<IMultiresult<ICountries>>) => {
+        if (dataResponse?.status) {
+          this.country = dataResponse?.data?.records as ICountries[];
+          this.setFieldValue(this.country as any[], 'mcountry', '_id', 'mcountry_id');
+        //  if (this.contact?.mcountry_id) {
+        //   this.contact.mcountry = this.contact.mcountry_id;
+        // }
+        }
+        this.isLoadingCountry = false;
+      // this.isLoadingCountry.set(false);
+      });
+  }
+
+  //   setFieldValue(records: any[], fieldName: string, columnName: string, recordFilterName: string, disableField = false) {
+  //   if (this.contact && records?.length > 0) {
+  //     let selectedRecord =
+  //       records[
+  //       records?.findIndex(
+  //         (item) => item?.[columnName] == this.contact?.[recordFilterName]
+  //       )
+  //       ];
+  //     // this.dataForm.get(fieldName)!.setValue(selectedRecord);
+  //     // if (this.dataForm && disableField) {
+  //     //   this.dataForm.get(fieldName)!.disable({ onlySelf: true });
+  //     // }
+  //      // Since you're using [(ngModel)], just patch the value to contact
+  //   this.contact[fieldName] = selectedRecord;
+
+  //   // if you need to disable input dynamically, handle via a flag
+  //   if (disableField) {
+  //     // set a flag and use [disabled] on your template input
+  //     this.contact[`${fieldName}_disabled`] = true;
+  //   }
+  //   }
+  // }
+
+  setFieldValue(
+  records: any[],
+  fieldName: string,
+  columnName: string,
+  recordFilterName: string,
+  disableField = false
+) {
+  if (this.contact && records?.length > 0) {
+    let selectedRecord = records.find(
+      (item) => item?.[columnName] == this.contact?.[recordFilterName]
+    );
+
+    if (selectedRecord) {
+      this.contact[fieldName] = selectedRecord; // ngModel binds this to <ng-select>
+    }
+
+    // if (disableField) {
+    //   this.contact[`${fieldName}_disabled`] = true;
+    // }
+  }
+}
 
   onSubmit(form: NgForm) {
     if (form.valid) {
